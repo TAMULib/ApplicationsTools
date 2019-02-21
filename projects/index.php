35e9901a4f9d90df7971807c5e146fb3638433bb
Michael Nichols <mumptastic@gmail.com>
2019-02-21 10:12:52 -0600

    table sorting with arrows

--- projects/index.php
+++ projects/index.php
@@ -106,7 +106,7 @@
 
   <section class="projects">
     <h2>All Projects</h2>
-    <table>
+    <table id="myTable">
       <thead>
         <tr>
           <th>Name</th>
@@ -135,40 +135,16 @@
 
 
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="js/tableSorter.js"></script>
     <script>
+        $(document).ready( function () {
+                $("#myTable").tablesorter({
+                      sortList: [[1,1]]
+                });
+        });
 
-      function sortTable(table, col, reverse) {
-          var tb = table.tBodies[0], // use `<tbody>` to ignore `<thead>` and `<tfoot>` rows
-              tr = Array.prototype.slice.call(tb.rows, 0), // put rows into array
-              i;
-          reverse = -((+reverse) || -1);
-          tr = tr.sort(function (a, b) { // sort rows
-              return reverse // `-1 *` if want opposite order
-                  * (a.cells[col].textContent.trim() // using `.textContent.trim()` for test
-                      .localeCompare(b.cells[col].textContent.trim())
-                    );
-          });
-          for(i = 0; i < tr.length; ++i) tb.appendChild(tr[i]); // append each row in order
-      }
+     
 
-      function makeSortable(table) {
-          var th = table.tHead, i;
-          th && (th = th.rows[0]) && (th = th.cells);
-          if (th) i = th.length;
-          else return; // if no `<thead>` then do nothing
-          while (--i >= 0) (function (i) {
-              var dir = 1;
-              th[i].addEventListener('click', function () {sortTable(table, i, (dir = 1 - dir))});
-          }(i));
-      }
-
-      function makeAllSortable(parent) {
-          parent = parent || document.body;
-          var t = parent.getElementsByTagName('table'), i = t.length;
-          while (--i >= 0) makeSortable(t[i]);
-      }
-
-      window.onload = function () {makeAllSortable();};
 
     </script>
 
