66529389964d9667a642198b463a584a515c6b5d
Michael Nichols <mumptastic@gmail.com>
2021-01-21 15:03:42 -0600

    added di calendar

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <div class="container">
+<div class="container">
         <div class="global-header">
             <a href="./"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2283/Libraries_white.svg" alt="Texas A&amp;M Libraries" /></a>
             <div class="date-time">
@@ -21,48 +21,48 @@
             <div class="links">
                 
                 <div class="link">
-                    <a href="./whiteboard">
-                    <h3>Whiteboard</h3>
-                    <h4>DI Digital Project Whiteboard</h4>
-                </a>
+                        <a href="./whiteboard">
+                        <h3>Whiteboard</h3>
+                        <h4>DI Digital Project Whiteboard</h4>
+                    </a>
+                </div>
+                
+                
+                <div class="link">
+                    <a href="./projects">
+                        <h3>Projects</h3>
+                        <h4>List of current projects and sprints</h4>
+                    </a>
+                </div>
+        
+        
+                <div class="link">
+                    <a href="https://demos.library.tamu.edu/component-library/" target="_blank">
+                        <h3>Design Library</h3>
+                        <h4>List of current designs for TAMU Libraries</h4>
+                    </a>
+                </div>
+    
+    
+                <div class="link">
+                    <a href="https://tamulib.github.io/tamu-library-components/docs/usage/" target="_blank">
+                        <h3>TL Components</h3>
+                        <h4>List of current components for TAMU Libraries</h4>
+                    </a>
+                </div>
+
+
+                <div class="link">
+                    <a href="https://hub.library.tamu.edu/Groups/Units/UserServices/DigitalInitiatives/Applications/ApplicationsPrivate/Lists/Main%20Calendar/calendar.aspx" target="_blank">
+                        <h3>DI Calendar</h3>
+                        <h4>Calendar of upcoming sprints/demos and employee leave</h4>
+                    </a>
+                </div>
+
             </div>
-            
-            
-            <div class="link">
-                <a href="./projects">
-                <h3>Projects</h3>
-                <h4>List of current projects and sprints</h4>
-            </a>
         </div>
-        
-        
-        <div class="link">
-            <a href="https://demos.library.tamu.edu/component-library/" target="_blank">
-            <h3>Design Library</h3>
-            <h4>List of current designs for TAMU Libraries</h4>
-        </a>
-    </div>
-    
-    
-    
-    <div class="link">
-        <a href="https://tamulib.github.io/tamu-library-components/docs/usage/" target="_blank">
-        <h3>TL Components</h3>
-        <h4>List of current components for TAMU Libraries</h4>
-    </a>
 
-    <div class="link">
-        <a href="https://hub.library.tamu.edu/Groups/Units/UserServices/DigitalInitiatives/Applications/ApplicationsPrivate/Lists/Main%20Calendar/calendar.aspx" target="_blank">
-        <h3>DI Calendar</h3>
-        <h4>Calendar of upcoming sprints/demos and employee leave</h4>
-    </a>
-
-    
-</div>
-
-</div>
-</div>
-
+            
 </div>
 </body>
 </html>
